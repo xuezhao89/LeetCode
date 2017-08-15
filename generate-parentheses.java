5df0ca3c00d326bb7aabfdb4ae15ddad3a2176ba
Xue Zhao <zhao.xue.work@gmail.com>
2017-08-14 23:27:06 -0700

    Aug.14th

---
+++ generate-parentheses.java
@@ -0,0 +1,47 @@
+﻿/*
+题目：
+思路：通过向string插入“（”和“）”直到两者数量都为n，则视为一个combination构建完成；
+生成“（”的条件是：只要“（”的数量没到n，就可以插入“（”；
+可以插入“）”的前提是：当前的“（”数量大于当前的“）”数量
+递归终止条件：“（”和“）”的数量相等，且等于n
+注意：这种问题不可能按照"顺序"结构进行罗列的,一定是用到了递归"做了一部分事情,剩下的递归去做"的核心；
+"穷举"这种情况,最合适的就是"回溯法"；
+这里应该是对回溯法的一个扩展(右括号的数量永远小于等于左括号)。
+思考：
+*/
+
+
+
+public class Solution {
+    /**
+     * @param n n pairs
+     * @return All combinations of well-formed parentheses
+     */
+    public ArrayList<String> generateParenthesis(int n) {
+        // Write your code here
+        ArrayList<String> result = new ArrayList<String>();
+        if (n <= 0) {
+            return result;
+        }
+        helper(result, "", n, n);
+        return result;
+    }
+    
+    public void helper(ArrayList<String> result,
+	                   String paren, // current parentheses
+	                   int left,     // how many left paren we need to add
+	                   int right) {  // how many right paren we need to add
+	if (left == 0 && right == 0) {
+		result.add(paren);
+		return;
+	}
+
+        if (left > 0) {
+		    helper(result, paren + "(", left - 1, right);
+        }
+        
+        if (right > 0 && left < right) {
+		    helper(result, paren + ")", left, right - 1);
+        }
+    }
+}
---
+++ valid-parentheses.java
@@ -0,0 +1,40 @@
+﻿
+/*
+题目：
+思路：遍历字符串的每一个元素时进行如下判断：遇见左括号就把对应的右括号加进栈，遇见右括号就进行如下判断；如果栈为空，说明之前没有遇见其对应的左括号，所以返回错误；如果栈不为空，判断出栈元素是否和当前括号一致，不一致也返回错误。
+注意：遍历结束后要最后检查stack是否为空，不为空返回错误。
+思考：
+*/
+public class Solution {
+    /**
+     * @param s A string
+     * @return whether the string is a valid parentheses
+     */
+    public boolean isValidParentheses(String s) {
+        // Write your code here
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        Map<Character, Character> pairMap = new HashMap<Character, Character>();
+        pairMap.put('(', ')');
+        pairMap.put('[', ']');
+        pairMap.put('{', '}');
+        
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(pairMap.get(c));
+            } else if (c == ')' || c == ']' || c == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char c2 = stack.pop();
+                if (c2 != c) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
