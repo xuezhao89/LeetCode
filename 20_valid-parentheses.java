d19f835675c649713d4029a0ac7d5b23fe9ccb53
xuezhao89 <blue.icy.summer@gmail.com>
2017-08-15 11:48:32 -0700

    add notes

--- 20_valid-parentheses.java
+++ 20_valid-parentheses.java
@@ -1,8 +1,17 @@
 ﻿
 /*
 题目：
-思路：遍历字符串的每一个元素时进行如下判断：遇见左括号就把对应的右括号加进栈，遇见右括号就进行如下判断；如果栈为空，说明之前没有遇见其对应的左括号，所以返回错误；如果栈不为空，判断出栈元素是否和当前括号一致，不一致也返回错误。
-注意：遍历结束后要最后检查stack是否为空，不为空返回错误。
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+思路：
+新建HashMap保存括号对应关系，新建Stack；
+遍历字符串的每一个元素，遇见左括号就把对应的右括号加进栈，遇见右括号判断：1.如果栈为空，说明之前没有遇见其对应的左括号，返回错误；2.如果栈不为空，判断出栈元素是否和当前括号一致，不一致也返回错误。
+
+注意：
+当遇见右括号时，先检查Stack是否为空，为空则返回错误；
+遍历结束后，最后检查Stack是否为空，不为空返回错误。
+
 思考：
 */
 public class Solution {
