76fbd3bee1ae39c9d39460a1cc95c12f054740d2
xuezhao89 <blue.icy.summer@gmail.com>
2017-08-16 16:00:32 -0700

    Update permutation-in-string.java

--- permutation-in-string.java
+++ permutation-in-string.java
@@ -1,5 +1,5 @@
 /*
-ÌâÄ¿£º
+é¢˜ç›®ï¼š
 Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the first string's permutations is the substring of the second string.
 Example 1:
 Input:s1 = "ab" s2 = "eidbaooo"
@@ -12,8 +12,8 @@ Note:
 The input strings only contain lower case letters.
 The length of both given strings is in range [1, 10,000].
 
-Ë¼Â·£º
-²Î¿¼find-all-anagrams-in-a-string
+æ€è·¯ï¼š
+å‚è€ƒfind-all-anagrams-in-a-string
 */
 
 public class Solution {
